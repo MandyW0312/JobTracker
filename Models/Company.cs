bb11beb0f2c6f7f940ec9e53becdd7c06424f990
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-01 18:52:30 -0500

    Defined a Model for the Company entity
    Made properties for each field we want our database to have

---
+++ Models/Company.cs
@@ -0,0 +1,28 @@
+namespace JobTracker.Models
+{
+    public class Company
+    {
+        //   Id
+        public int Id { get; set; }
+        //   Position (Position: text)
+        public string Position { get; set; }
+        //   Technology (Technology: text)
+        public string Technology { get; set; }
+        //   Contact Name (ContactName: text)
+        public string ContactName { get; set; }
+        //   Contact Email (ContactEmail: text)
+        public string ContactEmail { get; set; }
+        //   Remote (T/F) (Remote: boolean)
+        public bool Remote { get; set; }
+        //   Contact Count (ContactCount: int)
+        public int ContactCount { get; set; }
+        //   Name of the Company (CompanyName: text)
+        public string CompanyName { get; set; }
+        //   Salary (Salary: int)
+        public int Salary { get; set; }
+        //   Benefits (T/F) (Benefits: boolean)
+        public bool Benefits { get; set; }
+        //   Company Logo (CompanyLogo: text)    
+        public string CompanyLogo { get; set; }
+    }
+}
